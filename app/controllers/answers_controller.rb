740ca6d6e41dcd373169ed81de235d3dcae7d935
IchikiRyo <tohodaiichiki@gmail.com>
2017-03-31 00:42:07 +0900

    質問に答えられるようにしました。

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -3,7 +3,7 @@ class AnswersController < ApplicationController
   def new
     @question = Question.find(params[:question_id])
     @answer = Answer.new
-    @answer.question_id
+    @answer.question_id = @question.id
   end
 
   def create
